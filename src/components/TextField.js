1f95f8a110cf52b529e28ce652ab70bdc5fd2ed5
sabaijung <ardsri.s@gmail.com>
2022-01-31 11:45:02 +0700

    -

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,22 +1,35 @@
 import React from "react";
 import { ErrorMessage, useField } from "formik";
 
-export const TextField = ({ title, textarea = false, showLable = true, rows = 5, ...props }) => {
+export const TextField = ({
+  title,
+  textarea = false,
+  showLable = true,
+  rows = 5,
+  ...props
+}) => {
   const [field, meta] = useField(props);
   return (
     <div className="pr-2 mb-2 field-group">
       {showLable ? (
-        <label className="field-labe truncate"  htmlFor={field.name}>
+        <label className="field-labe truncate" htmlFor={field.name}>
           {title}
         </label>
       ) : (
         <></>
       )}
       {textarea ? (
-        <textarea className={`textarea ${meta.touched && meta.error && "is-invalid"}`} {...field} {...props} rows={rows} />
+        <textarea
+          className={`textarea ${meta.touched && meta.error && "is-invalid"}`}
+          {...field}
+          {...props}
+          rows={rows}
+        />
       ) : (
         <input
-          className={`field-input ${meta.touched && meta.error && "is-invalid"}`}
+          className={`field-input ${
+            meta.touched && meta.error && "is-invalid"
+          }`}
           {...field}
           {...props}
           autoComplete="off"
