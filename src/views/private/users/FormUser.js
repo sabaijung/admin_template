36f3ed98c1de0ca1875d61f223d7768d523f97af
sabaijung <ardsri.s@gmail.com>
2022-01-31 09:30:03 +0700

    หน้าจอบันทึกผู้ใช้งาน : เพิ่ม title ของหน้าจอ

--- src/views/private/users/FormUser.js
+++ src/views/private/users/FormUser.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function FormUser() {
   return (
     <div>
-      <h1>เพิ่มข้อมูลผู้ใช้งาน</h1>
+      <div className="flex flex-wrap mx-auto">
+        <div className="w-full pr-2 md:w-1/2">
+          <h1 className="text-blue-500">เพิ่มข้อมูลผู้ใช้งาน</h1>
+        </div>
+      </div>
+      <div className="mt-2 bg-white border-b border-l border-r border-gray-400" />
     </div>
   );
 }
