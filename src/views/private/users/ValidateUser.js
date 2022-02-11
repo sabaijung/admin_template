90c26c4391a77adc0d58330f06242874e6157455
sabaijung <ardsri.s@gmail.com>
2022-02-11 14:24:27 +0700

    เพิ่ม validate field

--- src/views/private/users/ValidateUser.js
+++ src/views/private/users/ValidateUser.js
@@ -1,6 +1,8 @@
 import { string, object, ref } from "yup";
 
 const emailValidation = /^[a-zA-Z0-9_\\.]+@[a-zA-Z]+\.[a-zA-Z0-9\\.]+$/;
+const passwordValidation =
+  /^(?=.*[A-Z-a-z])(?=.*\d)(?=.*[@$!%*#?&._])[A-Za-z\d@$!%*#?&._]{6,}$/;
 
 export const ValidateUser = object({
   prefix: string().required("กรุณาเลือกคำนำหน้าชื่อ"),
@@ -17,4 +19,17 @@ export const ValidateUser = object({
   username: string()
     .required("กรุณากรอกอีเมล")
     .matches(emailValidation, "รูปแบบผู้ใช้งานไม่ถูกต้อง"),
+  password: string()
+    .required("กรุณากรอกรหัสผ่าน")
+    .matches(
+      passwordValidation,
+      "รหัสผ่านต้องประกอบไปด้วย ตัวอักษรภาษาอังกฤษตัวพิมพ์ใหญ่(A-Z) ตัวอักษรภาษาอังกฤษตัวพิมพ์เล็ก(a-z) ตัวเลขอารบิก (0-9) และอักขระพิเศษ รวมกันอย่างน้อย 6 ตัว"
+    ),
+  confirmPassword: string()
+    .oneOf([ref("password"), null], "รหัสผ่านไม่ตรงกัน")
+    .required("กรุณากรอกยืนยันรหัสผ่าน")
+    .matches(
+      passwordValidation,
+      "รหัสผ่านต้องประกอบไปด้วย ตัวอักษรภาษาอังกฤษตัวพิมพ์ใหญ่(A-Z) ตัวอักษรภาษาอังกฤษตัวพิมพ์เล็ก(a-z) ตัวเลขอารบิก (0-9) และอักขระพิเศษ รวมกันอย่างน้อย 6 ตัว"
+    ),
 });
