45114f510bd5dadfc4b406d771a1aaa740a4e716
sabaijung <ardsri.s@gmail.com>
2022-02-28 10:34:44 +0700

    เพิ่มคำสั่ง

--- src/layout/public/PublicLayout.js
+++ src/layout/public/PublicLayout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Authen from '../../views/public/authentication/Authen';
 
 export default function PublicLayout(props) {
     return (
         <div>
-            <div className="w-full h-screen flex items-center justify-center bg-pastel-blue-50">{props.children}</div>
+            <div className="w-full h-screen flex items-center justify-center bg-pastel-blue-50"><Authen /></div>
         </div>
     );
 }
