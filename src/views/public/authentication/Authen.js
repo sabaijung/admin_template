a4ce0eb378b019f2f4c4c324e9d04c7e8527e13f
sabaijung <ardsri.s@gmail.com>
2022-02-28 10:34:21 +0700

    เพิ่มคำสั่ง

--- src/views/public/authentication/Authen.js
+++ src/views/public/authentication/Authen.js
@@ -18,18 +18,19 @@ export default function Authen() {
         >
             {({ errors, touched, values, handleChange, setFieldValue }) => (
                 <Form>
-                    <div>
-                        <div className="px-12 pb-10 my-2">
-                            <div className="w-full mb-2">
-                                <div className="py-2">
-                                    <TextField
-                                        name="username"
-                                        title="ชื่อผู้ใช้งาน"
-                                        type="text"
-                                        onChange={handleChange}
-                                        value={values.plainingName}
-                                    />
-                                    {/* <input
+                    <div className="w-full rounded-lg shadow-md  bg-gray-50">
+                        <div className="flex justify-center my-10 font-bold">
+                            <div className="px-12 pb-10 my-2">
+                                <div className="w-full mb-2">
+                                    <div className="py-2">
+                                        <TextField
+                                            name="username"
+                                            title="ชื่อผู้ใช้งาน"
+                                            type="text"
+                                            onChange={handleChange}
+                                            value={values.plainingName}
+                                        />
+                                        {/* <input
                                         value={values.username}
                                         type="text"
                                         placeholder="อีเมล"
@@ -39,18 +40,18 @@ export default function Authen() {
                                         }}
                                     />
                                     {errors.username && touched.username ? <div className="mt-1 input-error">{errors.username}</div> : null} */}
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="w-full mb-2">
-                                <div className="py-2">
-                                    <TextField
-                                        name="password"
-                                        title="รหัสผ่าน"
-                                        type="password"
-                                        onChange={handleChange}
-                                        value={values.plainingName}
-                                    />
-                                    {/* <input
+                                <div className="w-full mb-2">
+                                    <div className="py-2">
+                                        <TextField
+                                            name="password"
+                                            title="รหัสผ่าน"
+                                            type="password"
+                                            onChange={handleChange}
+                                            value={values.plainingName}
+                                        />
+                                        {/* <input
                                         value={values.password}
                                         type="password"
                                         id="input-pass"
@@ -60,18 +61,20 @@ export default function Authen() {
                                             setFieldValue("password", e.target.value);
                                         }}
                                     /> */}
+                                    </div>
                                 </div>
+                                <a href="/#" className="float-right mb-4 text-xs text-gray-500">
+                                    ลืมรหัสผ่าน?
+                                </a>
+                                <button className="mr-1 btn btn-green btn-sm">
+                                    เข้าสู่ระบบ
+                                </button>
                             </div>
-                            <a href="/#" className="float-right mb-4 text-xs text-gray-500">
-                                ลืมรหัสผ่าน?
-                            </a>
-                            <button className="mr-1 btn btn-green btn-sm">
-                                เข้าสู่ระบบ
-                            </button>
                         </div>
                     </div>
                 </Form>
-            )}
-        </Formik>
+            )
+            }
+        </Formik >
     )
 }
