6e20dac2418e5dead5ec112034fed79d19e2e73b
sabaijung <ardsri.s@gmail.com>
2022-02-28 09:36:33 +0700

    เพิ่มไฟล์หน้าจอล็อกอิน

---
+++ src/views/public/authentication/Authen.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Authen() {
+    return (
+        <div>
+            <div className="px-12 pb-10 my-2">
+                <div className="w-full mb-2">
+                    <div className="py-2">
+                        fsdfsd
+                    </div>
+                </div>
+                <div className="w-full mb-2">
+                    <div className="py-2">
+                        fsdfsd
+                    </div>
+                </div>
+                <a href="/#" className="float-right mb-4 text-xs text-gray-500">
+                    ลืมรหัสผ่าน?
+                </a>
+                <button type="submit" className="btn-login-2">
+                    fsdfsdf
+                </button>
+            </div>
+        </div>
+    )
+}
