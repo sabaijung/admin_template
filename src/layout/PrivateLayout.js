c3a4da3f7d640e768636c0825b56738b86b841ee
sabaijung <ardsri.s@gmail.com>
2022-02-28 10:34:33 +0700

    เพิ่มคำสั่ง

--- src/layout/PrivateLayout.js
+++ src/layout/PrivateLayout.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
+import Authen from "../views/public/authentication/Authen";
 import MainUser from "../views/private/users/MainUser";
 import FormUser from "../views/private/users/FormUser";
 import MainProject from "../views/private/project/MainProject"
@@ -21,7 +22,8 @@ export default function PrivateLayout() {
           className="p-4 break-words bg-white rounded-md shadow-xl"
         >
           <Switch>
-            <Route exact path="/MainUser" component={MainUser} />
+            <Route exact path="/Authentication" component={Authen} />
+            <Route path="/MainUser" component={MainUser} />
             <Route path="/MainUser/FormUser" component={FormUser} />
             <Route path="/MainProject" component={MainProject} />
             <Route path="/Project/FormProject" component={FormProject} />
