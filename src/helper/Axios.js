ba5aed5e6d959495fced70c000f3b9b25680cc9a
sabaijung <ardsri.s@gmail.com>
2022-03-25 13:37:43 +0700

    เพิ่มไฟล์

---
+++ src/helper/Axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: "https://localhost:44318/api/",
+  headers: {
+    'Content-Type': 'application/json',
+    //Authorization: "",
+  },
+});
+
