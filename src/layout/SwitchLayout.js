4e451c7610d0a84d9d544ae531b884ae98c9296a
sabaijung <ardsri.s@gmail.com>
2022-04-11 09:52:50 +0700

    ปรับปรุงโค้ด

--- src/layout/SwitchLayout.js
+++ src/layout/SwitchLayout.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { Route } from "react-router";
 
-export default function SwitchLayout({
-  component: Component,
-  layout: Layout,
-  ...rest
-}) {
+export default function SwitchLayout({ component: Component, layout: Layout, ...rest }) {
   return (
     <Route
       {...rest}
