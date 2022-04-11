1bb90973334aec4d79c960099ba03d6bab1ac55c
sabaijung <ardsri.s@gmail.com>
2022-04-11 09:46:21 +0700

    ปรับปรุงโค้ด

--- src/index.js
+++ src/index.js
@@ -1,36 +1,15 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./App";
-// import { Provider } from "react-redux";
-// //import store from './stores/Store';
-// import reportWebVitals from "./reportWebVitals";
-
-// ReactDOM.render(
-//   <Provider>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
 import React from "react";
 import ReactDOM from "react-dom";
-//import "./index.css";
-//import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./style/tailwind.css";
-import PrivateLayout from "./layout/PrivateLayout";
-import reportWebVitals from "./reportWebVitals";
+import { Provider } from 'react-redux';
+import store from './stores/Store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <PrivateLayout />
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>,
   </React.StrictMode>,
   document.getElementById("root")
 );
